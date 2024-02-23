2fb8ab7b1f3c617bfc01487d68762e502fe25077
hans112 <hansdevops127@gmail.com>
2024-02-23 21:29:17 +0100

    i wrote second print in eclips

--- src/main/java/com/main/TahshienProjec.java
+++ src/main/java/com/main/TahshienProjec.java
@@ -4,6 +4,7 @@ public class TahshienProjec {
 
 	public static void main(String[] args) {
 		System.out.println("Tah this one is only for test");
+		System.out.println("i builded new repo and i push it in eclips as well in github.");
 
 	}
 
