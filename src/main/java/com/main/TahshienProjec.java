c19d1af29cf6343f7947eac4aa9c8fe0ac14ce95
hans112 <hansdevops127@gmail.com>
2024-02-24 22:16:08 +0100

    develop not finding

--- src/main/java/com/main/TahshienProjec.java
+++ src/main/java/com/main/TahshienProjec.java
@@ -5,6 +5,9 @@ public class TahshienProjec {
 	public static void main(String[] args) {
 		System.out.println("Tah this one is only for test");
 		System.out.println("i builded new repo and i push it in eclips as well in github.");
+		System.out.println("i builded new repo and i push it in eclips as well in github.");
+		System.out.println("why Develop Branch desent work");
+
 
 	}
 
