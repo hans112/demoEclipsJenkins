31bf4a270adf33a89c7f8765aca9bc0c26b68b33
hans112 <hansdevops127@gmail.com>
2024-02-28 18:49:00 +0100

    overminn klm vriend

--- src/main/java/com/main/TahshienProjec.java
+++ src/main/java/com/main/TahshienProjec.java
@@ -5,7 +5,7 @@ public class TahshienProjec {
 	public static void main(String[] args) {
 		System.out.println("Tah this one is only for test");
 		System.out.println("i builded new repo and i push it in eclips as well in github.");
-
+  System.out.println("ik zag je een eerste dag dat je een goede jonge bent");
 	}
 
 }
